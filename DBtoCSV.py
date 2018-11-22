f58e85c366b63320d6eb5a76d5a5d2b424b1f2d7
Nazareno Montesoro <n.montesoro@gmail.com>
2018-11-22 16:27:40 -0300

    Commit inicial

---
+++ DBtoCSV.py
@@ -0,0 +1,67 @@
+import csv
+import sqlite3 as sql
+
+
+class cupones():
+    db = None
+
+    def __init__(self):
+        self.db = sql.connect('data.db')
+
+    def _CursorToCSV(self, cur, sep=',', old='', new=''):
+        csv_content = ''
+
+        for row in cur:
+            for cell in row:
+                csv_content += str(cell).replace(old, new) + sep
+            csv_content += '\n'
+
+        return csv_content
+
+    def _ParseCSV(self, cur):
+        return self._CursorToCSV(cur, sep=',', old=',', new=' ')
+
+    def GetCuponesPagados(self):
+        sql_pagados = '''SELECT * FROM export
+                         WHERE liquidado IS NOT NULL
+                         AND ABS(diferencia) < 0.5'''
+
+        cur = self.db.execute(sql_pagados).fetchall()
+
+        return self._ParseCSV(cur)
+
+    def GetCuponesNoPagados(self):
+        sql_no_pagados = '''SELECT * FROM export
+                            WHERE liquidado IS NULL'''
+
+        cur = self.db.execute(sql_no_pagados).fetchall()
+
+        return self._ParseCSV(cur)
+
+    def GetCuponesProblematicos(self):
+        # Cupones pagados parcialmente, o errores de carga
+
+        sql_problematicos = '''SELECT * FROM export
+                               WHERE liquidado IS NOT NULL
+                               AND ABS(diferencia) >= 0.5'''
+
+        cur = self.db.execute(sql_problematicos).fetchall()
+
+        return self._ParseCSV(cur)
+
+    def ExportCupones(self):
+        steps = {
+            'pagados.csv': self.GetCuponesPagados(),
+            'nopagados.csv': self.GetCuponesNoPagados(),
+            'problematicos.csv': self.GetCuponesProblematicos()
+        }
+
+        for filename, content in steps.items():
+            fp = open(filename, 'wt')
+            fp.write(content)
+            fp.close()
+
+
+if __name__ == '__main__':
+    c = cupones()
+    c.ExportCupones()
