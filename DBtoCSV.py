e30ad190a38afbd50e64307cd261e0b57e4b15ea
Nazareno Montesoro <n.montesoro@gmail.com>
2018-11-26 14:12:51 -0300

    Agrego opción para mostrar nombres de columnas

--- DBtoCSV.py
+++ DBtoCSV.py
@@ -18,11 +18,25 @@ class cupones():
             csv_content += '\n'
 
         return csv_content
+    
+    def _GetColumnNames(self, tablename):
+        cur = self.db.execute('PRAGMA TABLE_INFO(%s)' % (tablename))
+        colnames = []
+        for row in cur:
+            colnames.append(row[1])
+        return colnames
 
-    def _ParseCSV(self, cur):
+    def _ParseCSV(self, cur, tablename=''):
         fecha = 'Generado el ' + datetime.now().strftime(r'%Y-%m-%d')
-        csv_content = self._CursorToCSV(cur, sep=';', old='.', new=',')
-        csv_content = fecha + '\n' + csv_content
+        csv_content = fecha + '\n'
+
+        if not tablename == '':
+            colnames = self._GetColumnNames(tablename)
+            for col in colnames:
+                csv_content += col + ';'
+            csv_content += '\n'
+
+        csv_content += self._CursorToCSV(cur, sep=';', old='.', new=',')
         return csv_content
 
     def GetCuponesPagados(self):
@@ -32,7 +46,7 @@ class cupones():
 
         cur = self.db.execute(sql_pagados).fetchall()
 
-        return self._ParseCSV(cur)
+        return self._ParseCSV(cur, 'export')
 
     def GetCuponesNoPagados(self):
         sql_no_pagados = '''SELECT * FROM export
@@ -40,7 +54,7 @@ class cupones():
 
         cur = self.db.execute(sql_no_pagados).fetchall()
 
-        return self._ParseCSV(cur)
+        return self._ParseCSV(cur, 'export')
 
     def GetCuponesProblematicos(self):
         # Cupones pagados parcialmente, o errores de carga
@@ -51,7 +65,7 @@ class cupones():
 
         cur = self.db.execute(sql_problematicos).fetchall()
 
-        return self._ParseCSV(cur)
+        return self._ParseCSV(cur, 'export')
 
     def ExportCupones(self):
         steps = {
